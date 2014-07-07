b1923fc880bfdc0643b607ca0480188eaafc8479
ifaturoti_adeyemi <ifaturoti.adeyemi@gmail.com>
2014-07-07 17:05:32 +0200

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# chatrrBug Sample Application with Socket.IO, Twitter Bootstrap, Node.js, Express, JQuery and Redis#
+
+"ChatrrBug" is a sample CRUD application built with with Socket.IO, Twitter Bootstrap, Node.js, Express, JQuery and Redis.
+
+The application allows you to chat with multiple users in realtime connected to a node.js server via socket.io 
---
+++ app.js
@@ -0,0 +1,132 @@
+/**
+ * Module dependencies.
+ */
+//install redis
+var redis = require('redis');
+var redisClient = redis.createClient();
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+//app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// development only
+if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+var about = require('./routes/about');
+app.get('/about', about.about);
+
+
+var server = http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
+
+/*
+=================
+	socket.io ish
+=================
+*/
+var socket = require('socket.io');
+var io = socket.listen(server);
+
+
+//var handshakeData;
+var callCounter = 0;
+var storeMessage = function (name, data) {
+    var message = JSON.stringify({
+        name: name,
+        message: data
+    });
+    redisClient.lpush("messages", message, function () {
+        redisClient.ltrim("messages", 0, 9);
+    });
+};
+
+io.on('connection', function (client) {
+    console.log('Client connected...');
+
+    client.emit('start', {});
+
+    client.on('join', function (name) {
+        /*============== broadcast members================== **/
+        console.log(name + " just joined the chat");
+				client.username = name;
+
+
+        io.emit('add chatter', name);
+
+        redisClient.smembers('chatters', function (err, names) {
+
+            if (!err) {
+
+                names.forEach(function (name) {
+                    client.emit('add chatter', name);
+                });
+            } else {
+                console.log('the error here is ' + err);
+            }
+
+        });
+
+        redisClient.sadd("chatters", name);
+
+
+        /*================================================== **/
+
+
+        /*============== broadcast prev messages ================== **/
+        redisClient.lrange("messages", 0, -1, function (err, messages) {
+            messages = messages.reverse();
+            messages.forEach(function (message) {
+                var msg = JSON.parse(message);
+								console.log(msg);
+                client.emit("messages", msg);
+								//client.emit("messages", msg.name + " :" + msg.data);
+            });
+        });
+        /*================================================== **/
+
+
+
+
+
+    });
+
+    /*=============== handle disconnect ================== **/
+    client.on('disconnect', function () {
+        console.log("disconnecting "+client.username );
+        io.sockets.emit("remove chatter", client.username);
+        redisClient.srem("chatters", client.username);
+
+    });
+    /*================================================== **/
+
+    client.on("messages", function (data) {
+
+				io.emit("messages", data);
+				storeMessage(data.name,data.message);        
+
+    });
+
+});
